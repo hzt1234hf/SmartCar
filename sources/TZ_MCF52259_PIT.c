b806f747feebbb2f3c1e3ad45ad095b04cfc8116
hzt1234hf <hzt1234hf@163.com>
2015-11-02 22:09:19 +0800

    备份5_8位模式提取边线

--- sources/TZ_MCF52259_PIT.c
+++ sources/TZ_MCF52259_PIT.c
@@ -80,7 +80,9 @@ __declspec(interrupt:0) void TPIT0_interrupt(void){
 __declspec(interrupt:0) void TPIT1_interrupt(void){
 
 #if 0
-/*     8位操作实现提取边线   */
+/***********************************/
+/*      8位检测全图实现提取边线     */
+/***********************************/
     uint8 row,col,col2,data;
     for(row = 0; row<120; row++){
         for(col = 0,col2 = 1;col < 20; col++,col2++){
@@ -112,13 +114,17 @@ __declspec(interrupt:0) void TPIT1_interrupt(void){
             }
         }
     }
-    for()
     for(row = 0;row<40;row++){
         Image_Ptr[row * 60 + 9] |= 0x01;
         Image_Ptr[row * 60 + 10] |= 0x80;
     }
 #elif 1
-    uint8 row,col,col2,data;
+/***********************************/
+/*      8位扫描实现提取边线         */
+/***********************************/
+    vuint8 data;
+    vuint8 row,col;
+    vuint8 leftJP,rightJP;
     for(row = 0; row<120; row++){
         data = Image[row][0];
         for(col = 0;col < 19; col++){
@@ -126,32 +132,76 @@ __declspec(interrupt:0) void TPIT1_interrupt(void){
         }
         Image[row][col] = data;
     }
-    for(row = 0;row < 120;row++){
-        for(col = 9;col>=0;col--){
-            if(Image_Ptr[row * 20 + col] == 255)
-                break;
-            else if(Image_Ptr[row * 20 + col] == 0){
-                if(col <= 8 && Image_Ptr[row * 20 + col-1] == 255){
-                    Image_Ptr[row * 20 + col] = 0x80;
+    leftJP = 9;
+    rightJP = 10;
+    for(row = 119;;row--){
+        for(col = leftJP;col>=0;){
+            if(0&&Image[row][col] == 255){
+                leftJP = rightJP;           //如果出现这种情况，那么左边其实点与右边起始点对齐
+                continue;                   //略过此次循环，导致col并没有改变，从而实现重新检测这一行
+            }else if(Image[row][col] == 0){
+                if(col > 0 && Image[row][col-1] == 255){
+                    Image_Ptr[row * 20 + col-1] = 0x01;
+                    leftJP = col + 4;
+                    break;
+                }else if(col == 0){
+                    Image_Ptr[row * 20 + col] = 0x01;
                     break;
                 }
-            }
-
-                switch(data){
-                    case 0x01:Image_Ptr[row * 20 + col] = 0x01;break;
-                    case 0xFE:case 0x03:Image_Ptr[row * 20 + col] = 0x02;break;
-                    case 0xFC:case 0x07:Image_Ptr[row * 20 + col] = 0x04;break;
-                    case 0xF8:case 0x0F:Image_Ptr[row * 20 + col] = 0x08;break;
-                    case 0xF0:case 0x1F:Image_Ptr[row * 20 + col] = 0x10;break;
-                    case 0xE0:case 0x3F:Image_Ptr[row * 20 + col] = 0x20;break;
-                    case 0xC0:case 0x7F:Image_Ptr[row * 20 + col] = 0x40;break;
+            }else{
+                switch(Image[row][col]){
+                    case 0xFE:Image_Ptr[row * 20 + col] = 0x02;break;
+                    case 0xFC:Image_Ptr[row * 20 + col] = 0x04;break;
+                    case 0xF8:Image_Ptr[row * 20 + col] = 0x08;break;
+                    case 0xF0:Image_Ptr[row * 20 + col] = 0x10;break;
+                    case 0xE0:Image_Ptr[row * 20 + col] = 0x20;break;
+                    case 0xC0:Image_Ptr[row * 20 + col] = 0x40;break;
                     case 0x80:Image_Ptr[row * 20 + col] = 0x80;break;
-                    default:Image_Ptr[row * 20 + col] = 0xff;
                 }
+                break;
+                leftJP = col + 4;
+            }
+            col--;
         }
+        for(col = rightJP;col<=19;){
+            if(0&&Image[row][col] == 255){
+                rightJP = leftJP;           //如果出现这种情况，那么右边起始点与左边起始点对齐
+                continue;                   //略过此次循环，导致col并没有改变，从而实现重新检测这一行
+            }else if(Image[row][col] == 0){
+                if(col < 19 && Image[row][col+1] == 255){
+                    Image_Ptr[row * 20 + col+1] = 0x80;
+                    rightJP = col - 1;
+                    break;
+                }else if(col == 19){
+                    break;
+                }
+            }else{
+                switch(Image[row][col]){
+                    case 0x01:Image_Ptr[row * 20 + col] = 0x01;break;
+                    case 0x03:Image_Ptr[row * 20 + col] = 0x02;break;
+                    case 0x07:Image_Ptr[row * 20 + col] = 0x04;break;
+                    case 0x0F:Image_Ptr[row * 20 + col] = 0x08;break;
+                    case 0x1F:Image_Ptr[row * 20 + col] = 0x10;break;
+                    case 0x3F:Image_Ptr[row * 20 + col] = 0x20;break;
+                    case 0x7F:Image_Ptr[row * 20 + col] = 0x40;break;
+                }
+                break;
+                rightJP = col - 1;
+            }
+            col++;
+        }
+        if(row == 0)
+            break;
+        if( rightJP < leftJP ){//保证左右开始不会有交叉
+            rightJP = (rightJP + leftJP)/2;
+            leftJP = rightJP;
+        }
+
     }
 #elif 0
-/*     上位机显示原始采集图像   */
+/***********************************/
+/*     上位机显示偏移校正后的图像   */
+/***********************************/
     uint8 row,col,col2,data;
     for(row = 0; row<120; row++){
         for(col = 0,col2 = 1;col < 20; col++,col2++){
@@ -161,7 +211,9 @@ __declspec(interrupt:0) void TPIT1_interrupt(void){
         }
     }
 #elif 0
-/*     上位机显示图像   */
+/***********************************/
+/*     上位机显示原始采集的图像     */
+/***********************************/
     uint8 row,col,col2,data;
     for(row = 0; row<120; row++){
         for(col = 0,col2 = 1;col < 20; col++,col2++){
